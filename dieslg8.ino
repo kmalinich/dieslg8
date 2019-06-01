113a1bba2a7f9208cfd7ad37daab80abafa98023
kdm <kennygprs@gmail.com>
2019-06-01 13:19:25 -0400

    Update

--- dieslg8.ino
+++ dieslg8.ino
@@ -3,15 +3,49 @@
 #include <SPI.h>
 
 
-const int SPI_CS_CAN = 9;
-const int SPI_CS_SD  = 4;
+// Config: unit limits for gauges
+const int boost_psi_max = 40;
+const int coolant_c_max = 150;
 
-File log_file;
+// Config: Various value targets
+const int coolant_c_target = 75;
 
+// Config: disable/enable SD card logging
+bool logging_enable = 0;
+
+// Config: disable/enable automatic code clearing
+bool code_clear_all_enable      = 0;
+bool code_clear_specific_enable = 0;
+
+// Config: disable/enable turn signal LED flash when coolant temp reaches target
+bool temp_flash_enable = 0;
+
+// Config: disable/enable gauge sweep
+bool gauge_sweep_enable = 1;
+
+// Config: disable/enable gauge hijacking
+bool hijack_fuel_boost_enable   = 0;
+bool hijack_fuel_coolant_enable = 0;
+bool hijack_oil_boost_enable    = 0;
+bool hijack_oil_coolant_enable  = 1;
+
+// Config: step limits for gauges
+const int steps_max_large = 4667; // Large gauges (speedo, tach)
+const int steps_max_small = 1800; // Small gauges (fuel %, oil)
+
+
+// 1 psi = 68.9475729318 hPa
+// 1 hPA = 0.0145037738 psi
+const float hpa2psi = 68.9475729318;
+const float psi2hpa = 0.0145037738;
+
+// Ignition bitmask values to match against
 const int mask_ignition_acc = 0xC1; // buf[0]
 const int mask_ignition_run = 0xC5; // buf[0]
 const int mask_ignition_sta = 0xD5; // buf[0]
 
+
+// Status variables
 unsigned long loop_count_01 = 0;
 unsigned long loop_count_02 = 0;
 
@@ -20,35 +54,23 @@ unsigned long loop_count_02 = 0;
 // 2 = Pedal position + Engine RPM
 unsigned int data_expected = 0;
 
-// Step limits for gauges
-const int steps_max_large = 4667; // Large gauges (speedo, tach)
-const int steps_max_small = 1800; // Small gauges (fuel %, oil)
-
-// Unit limits for gauges
-const int boost_psi_max = 40;
-const int coolant_c_max = 150;
-
-// 1 psi = 68.9475729318 hPa
-// 1 hPA = 0.0145037738 psi
-const float hpa2psi = 68.9475729318;
-const float psi2hpa = 0.0145037738;
-
-
+// Integer status values
 unsigned int ambient_hpa;
 unsigned int engine_rpm;
 unsigned int boost_hpa_actual;
 unsigned int boost_hpa_target;
 
+// Float status values
 float boost_psi_actual;
 float boost_psi_target;
 float coolant_temp_c;
 float throttle_percent;
 
-
+// Boolean status values
 bool temp_flashed = 0;
 
-bool fuel_hijack_active = 0;
-bool oil_hijack_active  = 0;
+bool hijack_fuel_active = 0;
+bool hijack_oil_active  = 0;
 
 bool ignition_off = 1;
 bool ignition_acc = 0;
@@ -58,8 +80,16 @@ bool ignition_sta = 0;
 bool sweep_done = 0;
 
 
+// SPI pin config
+const int SPI_CS_CAN = 9;
+const int SPI_CS_SD  = 4;
+
 // Set CS pins
 MCP_CAN CAN(SPI_CS_CAN);
+
+// Declare File handle for logging
+File log_file;
+
 
 
 
@@ -96,7 +126,10 @@ void can_send(short address, byte a, byte b, byte c, byte d, byte e, byte f, byt
 // 4B73 : EGR engine exhaust heating control
 // 4CAE : EGR position sensor plausibility
 void code_clear() {
-	Serial.println("[dieslg8][CAN ][FUNC] Performing code clear specific");
+	// Return if disabled
+	if (code_clear_specific_enable == 0) return;
+
+	Serial.println("[dieslg8][CAN ][FUNC][DTC ][CLR ] Specific");
 
 	can_send(0x6F1, 0x12, 0x04, 0x18, 0x02, 0xFF, 0xFF, 0x00, 0x00); delay(75);
 
@@ -121,7 +154,10 @@ void code_clear() {
 
 // Clear all DDE codes
 void code_clear_all() {
-	Serial.println("[dieslg8][CAN ][FUNC] Performing code clear all");
+	// Return if disabled
+	if (code_clear_all_enable == 0) return;
+
+	Serial.println("[dieslg8][CAN ][FUNC][DTC ][CLR ] All");
 
 	turn_set(0x01);
 
@@ -135,9 +171,13 @@ void code_clear_all() {
 }
 
 void temp_flash() {
+	// Return if disabled
+	if (temp_flash_enable == 0) return;
+
+	// Return if already flashed
 	if (temp_flashed == 1) return;
 
-	Serial.println("[dieslg8][CAN ][FUNC] Performing coolant temp LED flash");
+	Serial.println("[dieslg8][CAN ][FUNC][DDE ][COOL] Turn LED flash");
 
 	turn_set(0x01); delay(111);
 	turn_set(0x03); delay(111);
@@ -162,123 +202,160 @@ void temp_flash() {
 // 0x02 : Right
 // 0x03 : Both
 void turn_set(unsigned int led_mask) {
+	Serial.println("[dieslg8][CAN ][FUNC][KOMB][TURN] Set");
+
 	can_send(0x6F1, 0x60, 0x05, 0x30, 0x2B, 0x06, led_mask, 0x04, 0x00);
 }
 
 // Reset turn signal LED(s) in the cluster
 void turn_reset() {
+	Serial.println("[dieslg8][CAN ][FUNC][KOMB][TURN] Reset");
+
 	can_send(0x6F1, 0x60, 0x03, 0x30, 0x2B, 0x00, 0x00, 0x00, 0x00);
 }
 
 
 // Illuminate active cruise LED(s) in the cluster
 // led_mask_1:
-// 0x01 : 
-// 0x02 : 
-// 0x03 : 
+// 0x01 :
+// 0x02 :
+// 0x03 :
 void acc_set() {
+	Serial.println("[dieslg8][CAN ][FUNC][KOMB][ACC ] Set");
+
 	can_send(0x6F1, 0x60, 0x03, 0x70, 0x27, 0x6A, 0x00, 0x00, 0x00);
 }
 
 // Reset active cruise LED(s) in the cluster
 void acc_reset() {
+	Serial.println("[dieslg8][CAN ][FUNC][KOMB][ACC ] Reset");
+
 	can_send(0x6F1, 0x60, 0x03, 0x70, 0x2B, 0x6E, 0x00, 0x00, 0x00);
 }
 
 
 void gauge_sweep() {
-	Serial.println("[dieslg8][CAN ][FUNC] Performing gauge sweep");
+	// Return if disabled
+	if (gauge_sweep_enable == 0) return;
+
+	Serial.println("[dieslg8][CAN ][FUNC][KOMB][SWP ] Performing");
 
 	// Speedo/tach to steps_max_large
-	gauge_hijack(0x20, steps_max_large);
-	gauge_hijack(0x21, steps_max_large);
+	hijack_gauge(0x20, steps_max_large);
+	hijack_gauge(0x21, steps_max_large);
 
 	// Fuel/oil to steps_max_small
-	fuel_hijack(steps_max_small);
-	oil_hijack(steps_max_small);
+	hijack_fuel(steps_max_small);
+	hijack_oil(steps_max_small);
 
 	delay(1000);
 
 	// Reset gauges
-	gauge_reset(0x20);
-	gauge_reset(0x21);
-	gauge_reset(0x22);
-	gauge_reset(0x23);
+	reset_gauge(0x20);
+	reset_gauge(0x21);
+	reset_gauge(0x22);
+	reset_gauge(0x23);
 }
 
 
-void fuel_hijack_boost() {
+void hijack_fuel_boost() {
+	// Return if disabled
+	if (hijack_fuel_boost_enable == 0) return;
+
 	// Return if less than 10 hPa
 	if (boost_hpa_actual < 10 || throttle_percent < 49) {
-		fuel_reset();
+		reset_fuel();
 		return;
 	}
 
 	unsigned int steps = boost_hpa_actual * (steps_max_small / (boost_psi_max * hpa2psi));
-	fuel_hijack(steps);
+	hijack_fuel(steps);
 }
 
-void fuel_hijack(unsigned int steps) {
-	// Return if steps are out of bounds
-	if (steps > steps_max_small) {
-		fuel_reset();
-		return;
-	}
+void hijack_fuel_coolant() {
+	// Return if disabled
+	if (hijack_fuel_coolant_enable == 0) return;
 
-	fuel_hijack_active = 1;
-	gauge_hijack(0x22, steps);
-}
-
-void fuel_reset() {
-	// Return if hijack is inactive
-	if (fuel_hijack_active == 0) return;
-	gauge_reset(0x22);
-	fuel_hijack_active = 0;
-}
-
-
-void oil_hijack_boost() {
-	// Return if less than 10 hPa
-	if (boost_hpa_target < 10) {
-		oil_reset();
-		return;
-	}
-
-	unsigned int steps = boost_hpa_target * (steps_max_small / (boost_psi_max * hpa2psi));
-	oil_hijack(steps);
-}
-
-void oil_hijack_coolant() {
 	// Return if less than 10 hPa
 	if (coolant_temp_c < 0 || coolant_temp_c > coolant_c_max) {
 		return;
 	}
 
 	unsigned int steps = coolant_temp_c * (steps_max_small / coolant_c_max);
-	oil_hijack(steps);
+	hijack_fuel(steps);
 }
 
-void oil_hijack(unsigned int steps) {
-	// Return if steps are out of bounds
-	if (steps > steps_max_small) {
-		oil_reset();
+
+void hijack_oil_boost() {
+	// Return if disabled
+	if (hijack_oil_boost_enable == 0) return;
+
+	// Return if less than 10 hPa
+	if (boost_hpa_target < 10) {
+		reset_oil();
 		return;
 	}
 
-	oil_hijack_active = 1;
-	gauge_hijack(0x23, steps);
+	unsigned int steps = boost_hpa_target * (steps_max_small / (boost_psi_max * hpa2psi));
+	hijack_oil(steps);
 }
 
-void oil_reset() {
+void hijack_oil_coolant() {
+	// Return if disabled
+	if (hijack_oil_coolant_enable == 0) return;
+
+	// Return if less than 10 hPa
+	if (coolant_temp_c < 0 || coolant_temp_c > coolant_c_max) {
+		return;
+	}
+
+	unsigned int steps = coolant_temp_c * (steps_max_small / coolant_c_max);
+	hijack_oil(steps);
+}
+
+
+void hijack_fuel(unsigned int steps) {
+	// Return if steps are out of bounds
+	if (steps > steps_max_small) {
+		reset_fuel();
+		return;
+	}
+
+	hijack_fuel_active = 1;
+	hijack_gauge(0x22, steps);
+}
+
+void hijack_oil(unsigned int steps) {
+	// Return if steps are out of bounds
+	if (steps > steps_max_small) {
+		reset_oil();
+		return;
+	}
+
+	hijack_oil_active = 1;
+	hijack_gauge(0x23, steps);
+}
+
+
+void reset_fuel() {
 	// Return if hijack is inactive
-	if (oil_hijack_active == 0) return;
-	gauge_reset(0x23);
-	oil_hijack_active = 0;
+	if (hijack_fuel_active == 0) return;
+
+	reset_gauge(0x22);
+	hijack_fuel_active = 0;
+}
+
+void reset_oil() {
+	// Return if hijack is inactive
+	if (hijack_oil_active == 0) return;
+
+	reset_gauge(0x23);
+	hijack_oil_active = 0;
 }
 
 
 // Hijack a gauge
-void gauge_hijack(int gauge_id, int steps) {
+void hijack_gauge(int gauge_id, int steps) {
 	uint8_t msb = (steps / 256); // MSB
 	uint8_t lsb = (steps % 256); // LSB
 
@@ -287,7 +364,7 @@ void gauge_hijack(int gauge_id, int steps) {
 
 
 // Un-hijack a gauge
-void gauge_reset(unsigned int gauge_id) {
+void reset_gauge(unsigned int gauge_id) {
 	// Gauge IDs
 	// 0x20 = Speedometer
 	// 0x21 = Tachometer
@@ -297,9 +374,10 @@ void gauge_reset(unsigned int gauge_id) {
 	can_send(0x6F1, 0x60, 0x03, 0x30, gauge_id, 0x00, 0xFF, 0xFF, 0xFF);
 }
 
+
 // Get data
-// 0x01, 0xF4 : [SPLAD]  Boost, target              hPA  x*0.091554
-// 0x07, 0x6D : [IPLAD]  Boost, actual              hPa  x*0.091554
+// 0x01, 0xF4 : [SPLAD]  Boost pressure, target     hPA  x*0.091554
+// 0x07, 0x6D : [IPLAD]  Boost pressure, actual     hPa  x*0.091554
 // 0x05, 0x47 : [ITKUM]  Coolant temp               C    (x/100)-100
 // 0x0C, 0x1C : [IPUMG]  Ambient pressure           hPa  x*0.030518
 // 0x01, 0x62 : [IFPWG]  Pedal position (filtered)  %    x*0.012207
@@ -314,9 +392,9 @@ void gauge_reset(unsigned int gauge_id) {
 // 0x0A, 0xF1 : [ITMOT]  Engine temp                        C  (x/10)-273.14
 // 0x0A, 0x8C : [ITOEL]  Oil temp                           C  (x/100)-100
 void status_messwertblock_lesen() {
-	// 0 = Ambient        + Boost actual
-	// 1 = Coolant temp   + Boost target
-	// 2 = Pedal position + Engine RPM
+	// 0 = Ambient pressure + Boost pressure actual
+	// 1 = Coolant temp     + Boost pressure target
+	// 2 = Pedal position   + Engine RPM
 	if (data_expected == 0) {
 		data_expected = 1;
 		// SPLAD + ITKUM
@@ -336,6 +414,9 @@ void status_messwertblock_lesen() {
 
 
 void sdcard_log() {
+	// Return if disabled
+	if (logging_enable == 0) return;
+
 	log_file = SD.open("logfile.csv", FILE_WRITE);
 
 	log_file.print(ignition_off);     log_file.print(",");
@@ -351,7 +432,7 @@ void sdcard_log() {
 
 	log_file.close();
 
-	Serial.println("[dieslg8][SD  ][WRIT] Wrote line");
+	// Serial.println("[dieslg8][SD  ][FUNC][WRIT][LINE] Done");
 }
 
 
@@ -363,29 +444,33 @@ void setup() {
 	Serial.begin(115200);
 
 	// Init CAN, baudrate 500k
-	while (CAN_OK != CAN.begin(CAN_500KBPS)) {
-		Serial.println("[dieslg8][CAN ][INIT] FAIL");
+	while (CAN.begin(CAN_500KBPS) != CAN_OK) {
+		Serial.println("[dieslg8][INIT][CAN ] Failure");
 		delay(1000);
 	}
 
-	Serial.println("[dieslg8][CAN ][INIT] OK");
+	Serial.println("[dieslg8][INIT][CAN ] OK");
 
-	// Init SD card
-	if (!SD.begin(SPI_CS_SD)) {
-		Serial.println("[dieslg8][SD  ][INIT] FAIL");
-		while(1);
+	// Initialize SD card logging
+	if (logging_enable == 1) {
+		if (!SD.begin(SPI_CS_SD)) {
+			Serial.println("[dieslg8][INIT][SD  ] Failure");
+			delay(1000);
+		}
+
+		Serial.println("[dieslg8][INIT][SD  ] OK");
 	}
-
-	Serial.println("[dieslg8][SD  ][INIT] OK");
 }
 
 void loop() {
 	unsigned char len = 0;
 	unsigned char buf[8];
 
+	// Increment loop counters
 	loop_count_01++;
 	loop_count_02++;
 
+	// Request status data every 1900 loops
 	if (loop_count_01 == 1900) {
 		loop_count_01 = 0;
 		if (ignition_run == 1) status_messwertblock_lesen();
@@ -393,7 +478,6 @@ void loop() {
 
 	if (loop_count_02 == 10000000) {
 		loop_count_02 = 0;
-		// if (ignition_run == 1) code_clear();
 	}
 
 	// Check if incoming data is available
@@ -422,7 +506,6 @@ void loop() {
 					if (ignition_run != 1) {
 						Serial.println("[dieslg8][IGN ][RUN ] Active");
 						gauge_sweep();
-						// code_clear();
 					}
 
 					ignition_off = 0;
@@ -437,7 +520,6 @@ void loop() {
 					if (ignition_run != 1) {
 						Serial.println("[dieslg8][IGN ][RUN ] Active");
 						gauge_sweep();
-						// code_clear();
 					}
 
 					ignition_off = 0;
@@ -451,8 +533,8 @@ void loop() {
 				if ((buf[0] & mask_ignition_acc) == mask_ignition_acc) {
 					if (ignition_acc != 1) {
 						Serial.println("[dieslg8][IGN ][ACC ] Active");
-						fuel_reset();
-						oil_reset();
+						reset_fuel();
+						reset_oil();
 					}
 
 					ignition_off = 0;
@@ -465,8 +547,8 @@ void loop() {
 				// By this point, ignition must be off
 				if (ignition_off != 1) {
 					Serial.println("[dieslg8][IGN ][OFF ] Active");
-					fuel_reset();
-					oil_reset();
+					reset_fuel();
+					reset_oil();
 				}
 
 				temp_flashed = 0;
@@ -486,14 +568,14 @@ void loop() {
 					unsigned int value_01 = (buf[4] << 8) | buf[5];
 					unsigned int value_02 = (buf[6] << 8) | buf[7];
 
-					ambient_hpa      = value_02 * 0.030518;
+					ambient_hpa      = value_01 * 0.030518;
 					boost_hpa_actual = (value_02 * 0.091554) - ambient_hpa;
 					boost_psi_actual = boost_hpa_actual / hpa2psi;
 
 					// Serial.print("[dieslg8][DATA][AMBh] "); Serial.println(ambient_hpa);
 					// Serial.print("[dieslg8][DATA][BSTa] "); Serial.println(boost_psi_actual);
 
-					fuel_hijack_boost();
+					hijack_fuel_boost();
 				}
 				else if (data_expected == 1) { // 1 = Coolant temp + Boost target
 					unsigned int value_01 = (buf[4] << 8) | buf[5];
@@ -504,13 +586,13 @@ void loop() {
 
 					coolant_temp_c = (value_02 * 0.01) - 100;
 
-					if (coolant_temp_c > 75) temp_flash();
+					if (coolant_temp_c > coolant_c_target) temp_flash();
 
 					// Serial.print("[dieslg8][DATA][BSTt] "); Serial.println(boost_psi_target);
 					// Serial.print("[dieslg8][DATA][CLTc] "); Serial.println(coolant_temp_c);
 
-					// oil_hijack_boost();
-					oil_hijack_coolant();
+					hijack_oil_boost();
+					hijack_oil_coolant();
 				}
 				else if (data_expected == 2) { // 2 = Pedal position + Engine RPM
 					unsigned int value_01 = (buf[4] << 8) | buf[5]; // Pedal
@@ -522,7 +604,7 @@ void loop() {
 					// Serial.print("[dieslg8][DATA][THRT] "); Serial.println(throttle_percent);
 					// Serial.print("[dieslg8][DATA][RPM ] "); Serial.println(engine_rpm);
 
-					fuel_hijack_boost();
+					hijack_fuel_boost();
 				}
 
 				break;
@@ -545,8 +627,6 @@ void loop() {
 
 			Serial.println();
 		}
-
-		sdcard_log();
 	}
 }
 
