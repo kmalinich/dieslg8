9e870aa6d2273d97c35c791881945cbcb5ad7fa1
kdm <kmalinich@users.noreply.github.com>
2021-04-20 11:48:30 -0400

    Update

--- upload.sh
+++ upload.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
-BOARD="arduino:avr:uno"
-OUTPUT="dieslg8.hex"
+BOARD="arduino:avr:leonardo"
+OUTPUT_DIR="${PWD}/build"
 
-PORT_DEF="$(find /dev/tty.usbmodem* 2> /dev/null | sed 's/\/dev\///g')"
-PORT="/dev/${1-$PORT_DEF}"
+PORT_DEF="$(serialport-list --format json | jq -r '.[] | select(.manufacturer != null) | select(.manufacturer | contains("Arduino")) | .path')"
+PORT="${1-$PORT_DEF}"
 
-rm -f "${OUTPUT}"
+# Install CAN-BUS Shield library if missing
+if ! arduino-cli lib list --format json | jq -r '.[].library.name' | grep -Eq 'CAN\-BUS_Shield'; then
+	if ! arduino-cli lib install 'CAN-BUS Shield'; then
+		echo "Failed to install missing library, cannot continue"
+		exit 1
+	fi
+fi
+
+
+rm -rf "${OUTPUT_DIR}"
+mkdir -p "${OUTPUT_DIR}"
 
 set -e
 
-arduino-cli compile --fqbn "${BOARD}" --output "${OUTPUT}" .
+arduino-cli compile --fqbn "${BOARD}" --output-dir "${OUTPUT_DIR}" ./
 echo
-arduino-cli upload --fqbn "${BOARD}" --input "${OUTPUT}" --port "${PORT}"
+
+arduino-cli upload --fqbn "${BOARD}" --port "${PORT}" ./
